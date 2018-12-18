64beb150f13bdba77e2ae51e3f3d7e46ffeacf8d
Mingyu Jo <owlsogul@naver.com>
2018-12-18 16:18:43 +0900

    [Client] Reader 변경

--- src/me/blog/owlsogul/jttp/server/client/Client.java
+++ src/me/blog/owlsogul/jttp/server/client/Client.java
@@ -7,6 +7,11 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonStreamParser;
+
 import me.blog.owlsogul.jttp.server.util.Log;
 
 public class Client implements Runnable{
@@ -15,7 +20,7 @@ public class Client implements Runnable{
 	private ClientController controller;
 	private Thread thread;
 	
-	private BufferedReader br;
+	private JsonStreamParser jsp;
 	private PrintWriter pw;
 	
 	public Client(ClientController controller, Socket socket) {
@@ -42,13 +47,20 @@ public class Client implements Runnable{
 	public void run() {
 
 		try {
-			br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			jsp = new JsonStreamParser(new InputStreamReader(socket.getInputStream(), "UTF-8"));
 			pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
-			
-			String request = br.readLine();
-			Log.info("%s에서 데이터를 보냈습니다: %s", toString(), request);
-			String response = controller.parseRequest(this, request);
-			sendString(response);
+			JsonArray requestArray = new JsonArray();
+			while (jsp.hasNext()) {
+				JsonElement request = jsp.next();
+				requestArray.add(request);
+				Log.info("%s에서 데이터를 보냈습니다: %s", toString(), request);
+				String response = controller.parseRequest(this, request.toString());
+				sendString(response);
+			}
+			pw.close();
+			socket.close();
+		} catch(JsonIOException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
