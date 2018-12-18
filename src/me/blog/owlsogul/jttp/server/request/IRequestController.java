ada59cabf302579767255d8ef99f07d1150b8d35
Mingyu Jo <owlsogul@naver.com>
2018-12-18 19:46:25 +0900

    [RequestController] 서버 확장하면서 페이지 추가 할 수 있게 인터페이스 수정

--- src/me/blog/owlsogul/jttp/server/request/IRequestController.java
+++ src/me/blog/owlsogul/jttp/server/request/IRequestController.java
@@ -1,10 +1,13 @@
 package me.blog.owlsogul.jttp.server.request;
 
 import me.blog.owlsogul.jttp.server.observer.client.ClientRequestObserver;
+import me.blog.owlsogul.jttp.server.request.exception.DuplicatePageException;
+import me.blog.owlsogul.jttp.server.request.page.RequestPage;
 
 public interface IRequestController {
 
 	public void addRequestObserver(ClientRequestObserver observer);
 	public void removeRequestObserver(ClientRequestObserver observer);
+	public void addRequestPage(String command, RequestPage requestPage) throws DuplicatePageException;
 	
 }
